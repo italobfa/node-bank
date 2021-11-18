dd66f14a043f402c36ab9084438436a80ba6a509
italobfa <italobfa@hotmail.com>
2021-11-17 21:45:58 -0300

    create middleware to verify account

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ const app = express();
 app.use(express.json());
 
 const clients = [];
+//Usando middleware para fazer a verificação de conta
+function verifyIfAccountExistsCPF(request, response, next) {
+  const { cpf } = request.headers;
+  //Procurando se existe o cpf passado no parametro, para poder retornar os dados necessarios, no caso o statement
+  const client = clients.find((client) => client.cpf === cpf);
+
+  if (!client) {
+    return response.status(400).json({ error: "Client not found" });
+  }
+  //Passando a informação criada no middleware e que será usada na rota.
+  request.client = client;
+
+  return next();
+}
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
@@ -26,15 +40,12 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
-  //Procurando se existe o cpf passado no parametro, para poder retornar os dados necessarios, no caso o statement
-  const client = clients.find((client) => client.cpf === cpf);
+/*Posso passar o middleware de duas formas, usando o app.use() e tudo que estiver abaixo vai passar pela verificação,
+ou passar direto na rota, e somente aquela rota irá usar o middleware.
+app.use(verifyIfAccountExistsCPF)*/
 
-  if(!client){
-    return response.status(400).json({error: "Client not found"})
-  }
-
+app.get("/statement", verifyIfAccountExistsCPF, (request, response) => {
+  const {client} = request
   return response.json(client.statement);
 });
 
