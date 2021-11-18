6eda01aee05b1c4402f77d4913f83aeb1349c8a1
italobfa <italobfa@hotmail.com>
2021-11-17 21:21:52 -0300

    validating client for deposit

--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid")
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const clients = []
+const clients = [];
 
 app.post("/account", (request, response) => {
-  const {cpf,name} = request.body
-  //buscando no array clients, se o cpf cadastrado ja existe ou nao.
-  const clientAlreadyExists = clients.some( client => client.cpf === cpf)
+  const { cpf, name } = request.body;
+  //buscando no array clients, se o cpf cadastrado já existe ou não.
+  const clientAlreadyExists = clients.some((client) => client.cpf === cpf);
 
-  if(clientAlreadyExists) {
-    return response.status(400).json({error: "Client already exists"})
+  if (clientAlreadyExists) {
+    return response.status(400).json({ error: "Client already exists" });
   }
 
   clients.push({
     cpf,
     name,
     id: uuidv4(),
-    statement: []
-  })
+    statement: [],
+  });
 
-  return response.status(201).send()
+  return response.status(201).send();
 });
 
 app.get("/statement/:cpf", (request, response) => {
   const { cpf } = request.params;
   //Procurando se existe o cpf passado no parametro, para poder retornar os dados necessarios, no caso o statement
-  const client = clients.find(client => client.cpf === cpf)
+  const client = clients.find((client) => client.cpf === cpf);
 
-  return response.json(client.statement)
-})
+  if(!client){
+    return response.status(400).json({error: "Client not found"})
+  }
+
+  return response.json(client.statement);
+});
 
 app.listen(3333);
